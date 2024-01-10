62b9e7b619f88eceacefcb7aca8857052564a3e6
anon121213 <anon121213@mail.ru>
2024-01-10 07:36:48 +0300

    new palette

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    /*private bool aimVisivle = false;*/
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mouseCursorPos;
+
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            Cursor.visible = true;
+            gameObject.SetActive(false);
+        }
+
+        /*if (aimVisivle == true)
+        {
+            Cursor.visible = true;
+            gameObject.SetActive(false);
+        }
+        else if (aimVisivle == false)
+        {
+            gameObject.SetActive(true);
+            Cursor.visible = false;
+        }*/
+    }
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,9 +5,17 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _speed;
     private Rigidbody2D _rigidbody;
 
-    void Start()
+    private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbody.velocity = transform.up * _speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Walls") 
+        {
+            Destroy(gameObject);
+        }
     }
 }
